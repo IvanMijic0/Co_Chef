2d81c8661f8143a619e13664ad3cf1cf312a036e
IvanMijic0 <ivanmijic159951@gmail.com>
2023-05-13 17:30:16 +0200

    Test Buttons

--- Co-Chef-Frontend/Control/controller.js
+++ Co-Chef-Frontend/Control/controller.js
@@ -4,9 +4,11 @@ import { sceneData } from "../Scenes/scene-data.js";
 const scenes = [
     createScene(sceneData.INTRO.canvasId, sceneData.INTRO.backgroundColor, sceneData.INTRO.image),
     createScene(sceneData.START_MENU.canvasId, sceneData.START_MENU.backgroundColor, sceneData.START_MENU.image),
+    createScene(sceneData.TEST.canvasId, sceneData.TEST.backgroundColor, sceneData.TEST.image)
 ];
 
 let activeScene = 0;
+let previousScene = 0;
 
 const drawActiveScene = () => {
     scenes[activeScene].draw();
@@ -15,16 +17,45 @@ const drawActiveScene = () => {
 
 drawActiveScene();
 
-document.getElementById("switchButton").addEventListener("click", () => {
+document.getElementById("switchButton-container").addEventListener("click", () => {
+    switchToScene(sceneData.START_MENU.sceneId);
+});
+
+document.getElementById("testButton-container").addEventListener("click", () => {
+    switchToScene(sceneData.TEST.sceneId);
+});
+
+document.getElementById("backButton-container").addEventListener("click", () => {
+    for (let i = 0; i < scenes.length; i++) {
+        if (activeScene === i && previousScene === i) {
+            previousScene = 0;
+            break;
+        }
+    }
+
     const currentScene = scenes[activeScene];
-    const nextScene = scenes[(activeScene + 1) % scenes.length];
+    const nextScene = scenes[previousScene];
 
     currentScene.hide();
     nextScene.show();
 
-    activeScene = (activeScene + 1) % scenes.length;
+    activeScene = previousScene;
 
-    console.log("Current scene -> " + currentScene.canvas.style.display);
-    console.log("Next scene -> " + nextScene.canvas.style.display);
-
+    console.log("Active scene -> " + activeScene);
+    console.log("Previous scene -> " + previousScene);
 });
+
+
+const switchToScene = (sceneId) => {
+    const currentScene = scenes[activeScene];
+    const nextScene = scenes[sceneId];
+
+    currentScene.hide();
+    nextScene.show();
+
+    previousScene = activeScene;
+    activeScene = sceneId;
+
+    console.log("Active scene -> " + activeScene);
+    console.log("Previous scene -> " + previousScene);
+};
--- Co-Chef-Frontend/Scenes/scene-data.js
+++ Co-Chef-Frontend/Scenes/scene-data.js
@@ -4,11 +4,19 @@ export const sceneData = {
         backgroundColor: "#FFD2BB",
         image: {element: document.getElementById("cookie"), width: 1024, height: 1024},
         image_width: 1024,
-        image_height: 1024
+        image_height: 1024,
+        sceneId: 0
     },
     START_MENU:  {
         canvasId: "start-menu-canvas",
         backgroundColor: "#FFD2BB",
         image: {element: document.getElementById("cookie2"), width: 1024, height: 1024},
+        sceneId: 1
+    },
+    TEST:  {
+        canvasId: "test-canvas",
+        backgroundColor: "#FFFFFF",
+        image: {element: document.getElementById("test"), width: 1920, height: 1080},
+        sceneId: 2
     }
 }
