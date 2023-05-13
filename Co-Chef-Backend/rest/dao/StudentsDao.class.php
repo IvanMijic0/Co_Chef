4ff6f4077dc973c0c5d44576130b04730a2d19fe
IvanMijic0 <ivanmijic159951@gmail.com>
2023-05-13 16:08:59 +0200

    Add Backend from web course

--- Co-Chef-Backend/rest/dao/StudentsDao.class.php
+++ Co-Chef-Backend/rest/dao/StudentsDao.class.php
@@ -8,15 +8,4 @@ class StudentsDao extends BaseDao
     {
         parent::__construct("students");
     }
-
-    // custom function, which is not present in BaseDao
-    // query_unique -> returns only 1 result if multiple are present
-    function getUserByFirstNameAndLastName($firstName, $lastName)
-    {
-        return $this->query_unique(
-            "SELECT * FROM students 
-                    WHERE firstName = :firstName AND lastName = :lastName",
-                          ["firstName" => $firstName, "lastName" => $lastName]
-        );
-    }
 }
