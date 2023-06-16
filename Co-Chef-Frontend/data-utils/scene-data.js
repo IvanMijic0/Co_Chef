305d5697fb7ea344168847a68d7e2db4fdfa0c4a
IvanMijic0 <ivanmijic159951@gmail.com>
2023-06-17 01:00:00 +0200

    Possibly working

--- Co-Chef-Frontend/data-utils/scene-data.js
+++ Co-Chef-Frontend/data-utils/scene-data.js
@@ -119,7 +119,7 @@ export const sceneData = {
         stir_item_new_transform: "33vh",
         fry_item_original_transform: "14vh",
         fry_item_new_transform: "44vh",
-        timerValue: 700000.2,
+        timerValue: 400000.2,
         sceneId: 9,
     },
 }
