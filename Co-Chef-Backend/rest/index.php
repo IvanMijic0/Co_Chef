81f8e3005636cff14d30b97114953ec2edda0f24
IvanMijic0 <ivanmijic159951@gmail.com>
2023-06-28 17:03:45 +0200

    Attempt to fix CORS policy

--- Co-Chef-Backend/rest/index.php
+++ Co-Chef-Backend/rest/index.php
@@ -6,6 +6,29 @@ require "dao/ChatsDao.php";
 require "services/UserService.php";
 require "services/ChatServices.php";
 
+// Allow requests from a specific origin
+header('Access-Control-Allow-Origin: https://monkfish-app-zvwdu.ondigitalocean.app');
+
+// Allow specific HTTP methods
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+// Allow specific headers (if needed)
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+// Allow credentials (if needed)
+header('Access-Control-Allow-Credentials: true');
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Set additional headers for preflight requests
+    header('Access-Control-Max-Age: 86400');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    exit;
+}
+
 Flight::register("user_service", UserService::class);
 Flight::register("chat_service", ChatServices::class);
 
