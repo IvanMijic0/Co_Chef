c2a7d90cf99708c6ccd629a2ea43df932be98adf
IvanMijic0 <ivanmijic159951@gmail.com>
2023-07-05 22:57:01 +0200

    Setting up swagger

--- Co-Chef-Backend/rest/routes/ApiSpecs.php
+++ Co-Chef-Backend/rest/routes/ApiSpecs.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @OA\Info(title="Co_Chef", version="0.1", @OA\Contact(email="ivan.mijic@stu.ibu.edu.ba", name="Ivan Mijić"))
+ * @OA\Info(title="Co-Chef", version="0.1", @OA\Contact(email="ivan.mijic@stu.ibu.edu.ba", name="Ivan Mijić"))
  * @OA\OpenApi(
  *     @OA\Server(url="http://localhost/Co_Chef/Co-Chef-Backend/rest", descritpion="Development Environment"),
  *     @OA\Server(url="https://shark-app-7dvmx.ondigitalocean.app/rest", descritpion="Production Environment")
