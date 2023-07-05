a8e2e4ae12e76752d7aa9b513b6eae8fc74a344e
IvanMijic0 <ivanmijic159951@gmail.com>
2023-07-06 00:49:33 +0200

    Setting up swagger for routes

--- Co-Chef-Backend/rest/index.php
+++ Co-Chef-Backend/rest/index.php
@@ -2,8 +2,10 @@
 
 use OpenApi\Analysis;
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-ini_set('display_errors', '1');
+
 
 require "../vendor/autoload.php";
 require "dao/UsersDao.php";
