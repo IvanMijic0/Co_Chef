4a57e7e3f0d6bdc9fe05a27d4054d4e13ae17679
IvanMijic0 <ivanmijic159951@gmail.com>
2023-07-05 23:45:42 +0200

    Setting up swagger for routes

--- Co-Chef-Backend/rest/index.php
+++ Co-Chef-Backend/rest/index.php
@@ -9,25 +9,25 @@ require "services/UserService.php";
 require "services/ChatServices.php";
 
 // Allow requests from a specific origin
-header('Access-Control-Allow-Origin: https://lionfish-app-zkpvm.ondigitalocean.app');
+header("Access-Control-Allow-Origin: https://lionfish-app-zkpvm.ondigitalocean.app");
 
 // Allow specific HTTP methods
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
 // Allow specific headers (if needed)
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 // Allow credentials (if needed)
-header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Credentials: true");
 
 // Handle preflight requests
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     // Set additional headers for preflight requests
-    header('Access-Control-Max-Age: 86400');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('Content-Length: 0');
-    header('Content-Type: text/plain');
+    header("Access-Control-Max-Age: 86400");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    header("Content-Length: 0");
+    header("Content-Type: text/plain");
     exit;
 }
 
@@ -46,9 +46,9 @@ require_once "routes/ChatsRoutes.php";
 
 
 Flight::before('error', function () {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE');
-    header('Access-Control-Allow-Headers: Content-Type');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE");
+    header("Access-Control-Allow-Headers: Content-Type");
 });
 
 Flight::before("json", function () {
