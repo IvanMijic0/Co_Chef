a80cf7058fcda244af6008fd5850625e224a7269
IvanMijic0 <ivanmijic159951@gmail.com>
2023-07-10 04:29:14 +0200

    Setting up JWT

--- Co-Chef-Backend/rest/routes/UserRoutes.php
+++ Co-Chef-Backend/rest/routes/UserRoutes.php
@@ -35,7 +35,15 @@ Flight::route('POST /login', function () {
     if (isset($user['id'])) {
         if ($user['userPassword'] == $login['password']) {
             unset($user['userPassword']);
-            $jwt = JWT::encode($user, Config::JWT_SECRET(), 'HS256');
+
+            $expirationTime = time() + 3600; // Token will expire in 1 hour (adjust as needed)
+
+            $payload = [
+                'exp' => $expirationTime,
+                'user' => $user,
+            ];
+
+            $jwt = JWT::encode($payload, Config::JWT_SECRET(), 'HS256');
             Flight::json(['user_token' => $jwt]);
         } else {
             Flight::json(["message" => "Wrong password"], 404);
